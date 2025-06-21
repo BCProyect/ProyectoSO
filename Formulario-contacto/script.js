0a2e2082a940b243630a70067f451f15746b5daf
BCProyect <bcproyecto3@gmail.com>
2025-06-20 22:31:57 -0600

    subida de Proyecto de Sistema operativo

---
+++ Formulario-contacto/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('¡Gracias por contactarnos! Tu mensaje ha sido enviado (demo).');
+  });
+  
